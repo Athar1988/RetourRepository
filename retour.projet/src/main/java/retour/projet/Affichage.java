a93123499f143c0a4f27d12c1d027046eb80995b
Athar1988 <hakim.athar.88@gmail.com>
2020-07-20 10:15:49 +0100

    ajouter mess	ge

--- retour.projet/src/main/java/retour/projet/Affichage.java
+++ retour.projet/src/main/java/retour/projet/Affichage.java
@@ -4,6 +4,8 @@ public class Affichage {
 
 	public void AffichageMessage() {
 		System.out.println("Helooo !!!!");
+		System.out.println("Les enfants !!!!");
+
 	}
 	public int somme(int a, int b) {
 		return a+b;
