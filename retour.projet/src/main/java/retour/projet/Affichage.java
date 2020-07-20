522e478dd13423e8a28d96f1b87dcdde5035198c
Athar1988 <hakim.athar.88@gmail.com>
2020-07-20 10:04:21 +0100

    Creation nouveau projet

---
+++ retour.projet/src/main/java/retour/projet/Affichage.java
@@ -0,0 +1,11 @@
+package retour.projet;
+
+public class Affichage {
+
+	public void AffichageMessage() {
+		System.out.println("Helooo !!!!");
+	}
+	public int somme(int a, int b) {
+		return a+b;
+	}
+}
---
+++ retour.projet/src/main/java/retour/projet/Message.java
@@ -0,0 +1,15 @@
+package retour.projet;
+
+public class Message {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Affichage A= new Affichage();
+		A.AffichageMessage();
+		int x=5;
+		int y=6;
+		System.out.println(A.somme(x, y));
+	}
+
+}
