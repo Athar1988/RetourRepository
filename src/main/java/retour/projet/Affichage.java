ba2cf50435fd6aae3ce38a2684e29da15c36d77a
Athar1988 <hakim.athar.88@gmail.com>
2020-07-21 08:25:15 +0100

    premier commit

---
+++ src/main/java/retour/projet/Affichage.java
@@ -0,0 +1,13 @@
+package retour.projet;
+
+public class Affichage {
+
+	public void AffichageMessage() {
+		System.out.println("Helooo !!!!");
+		System.out.println("Les enfants !!!!");
+
+	}
+	public int somme(int a, int b) {
+		return a+b;
+	}
+}
---
+++ src/test/java/retour/projet/TestretourMessage.java
@@ -0,0 +1,17 @@
+package retour.projet;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestretourMessage {
+	Affichage A= new Affichage();
+
+	@Test
+	public void test() {
+		Affichage A= new Affichage();
+
+assertEquals(10,A.somme(8, 2));
+	}
+
+}
